a80d6b2664c649ee70763f48449068b3b92598a6
Mahmoud Tantawy <mtantawy@users.noreply.github.com>
2015-01-15 22:57:53 +0200

    editing content

--- application/views/contact.php
+++ application/views/contact.php
@@ -18,17 +18,5 @@
               <strong>Telephone: </strong>01200199329 <br>
             </p>
 
-            <h3>6th of October Branch</h3>
-            <p>
-              <strong>Address: </strong>Sheikh Zayed City, Mogammaa al Thawra al khadra, infront of al Yasmine compound, Gate no. 4. <br>
-              <strong>Telephone: </strong>01008885454 <br>
-            </p>
-
-            <h3>Detailer’s Express Shop</h3>
-            <p>
-              <strong>Address: </strong>2, Khaled ibn el Waleed street, Sheraton Heliopolis, in front of the church. <br>
-              <strong>Telephone: </strong>02 22695758 <br>
-            </p>
-
           </div>
         </div>
